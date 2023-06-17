244cea16e989337f2afa09e78c3328377696332c
Freshkay <cruzzydollar@gmail.com>
2023-06-17 10:32:31 -0700

    software

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point of a program
+ * Return: Always 0 (success)
+ */
+int main()
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
