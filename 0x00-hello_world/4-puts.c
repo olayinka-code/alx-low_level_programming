0c063bf1f39c19f9eb08476a40cbb8c224f5106c
Freshkay <cruzzydollar@gmail.com>
2023-06-17 10:14:46 -0700

    software

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry piont of a program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
