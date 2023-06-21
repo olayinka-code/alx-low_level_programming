701134083de04c676ef366844040b065ebba4e88
Freshkay <cruzzydollar@gmail.com>
2023-06-21 03:33:26 -0700

    software

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/**
+	 * if: the if statement is use to analyze the fucntion
+	 *
+	 * printf: the printf function is use to print out the output to the stdo
+	 *
+	 */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
