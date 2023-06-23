b7d2e137583fc7d80647da506b9a21f0ba9aa398
Freshkay <cruzzydollar@gmail.com>
2023-06-23 14:11:58 -0700

    software

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	/* call the function print_alphabet m */
+	print_alphabet();
+	return (0);
+}
