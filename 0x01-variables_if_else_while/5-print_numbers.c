3f9f7a0e06a516305d5b007ec66285100b0a4fb9
Freshkay <cruzzydollar@gmail.com>
2023-06-23 13:23:47 -0700

    software

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main -entry point of a program
  *
  * return: always 0 (success)
  *
  */
+
 int main(void) 
 {
-    int i;
+	int i;
+	/**
+	 * for this function is used to loop the program till a condition is met
+	 *
+	 * printf: this function is used to print output to the stdo
+	 *
+	 */
 
     for (i = 0; i < 10; i++) {
         printf("%d\n", i);
