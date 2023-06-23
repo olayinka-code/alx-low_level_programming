e547833c668d2e866a7ac337c80e74b36136b5b8
Freshkay <cruzzydollar@gmail.com>
2023-06-23 13:03:28 -0700

    software

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -entry point of a program
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int A;
+	int B;
+	/**
+	 * for: this fucntion is used to loop the program till a condition is met
+	 *
+	 * putchar is used to print a single char to the std output
+	 *
+	 */
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		putchar(A);
+	}
+	for (B = 'A'; B <= 'Z'; B++)
+	{
+		putchar(B);
+	}
+	putchar('\n');
+	return (0);
+}
