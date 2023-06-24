e1609a0d064b07dde380797e2292a702b6e89dd6
Freshkay <cruzzydollar@gmail.com>
2023-06-24 13:09:20 -0700

    software

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+		int n;
+
+		n = add(89, 9);
+		printf("%d\n", n);
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ *
+ * Return Always 0
+ *
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
