75ca04f5b469126c3f4306d1eda7c65b73c5b583
Freshkay <cruzzydollar@gmail.com>
2023-07-28 09:41:52 -0700

    software

--- 0x12-singly_linked_lists/0x10-variadic_functions/0-sum_them_all.c
+++ 0x12-singly_linked_lists/0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,18 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list ap;
 	unsigned int i, total = 0;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_start(args, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		total += va_arg(args, int);
+		total += va_arg(ap, int);
 	}
-	va_end(args);
+	va_end(ap);
 	return (total);
 }
