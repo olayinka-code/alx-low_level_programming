d48e1c13bb6416e069642caf308e1c191ccf7ab9
olayinka-code <cruzzydollar@gmail.com>
2023-09-03 05:26:29 -0700

    software

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by newline to the stdout.
+ * @s: a pointer to the string
+ *
+ * Return: always 0 (succes)
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
