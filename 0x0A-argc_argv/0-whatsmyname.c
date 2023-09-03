1d0ff22437934aa01eb0d9782e60122f038b9bd6
olayinka-code <cruzzydollar@gmail.com>
2023-09-03 05:15:58 -0700

    software

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
